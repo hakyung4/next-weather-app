60b53ecea695070aa9495d6e0a36cde3c301c662
hakyung4 <hakyung4@illinois.edu>
2022-07-22 14:30:09 -0500

    fetching more data and display it

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["openweathermap.org"],
+  }
 }
 
 module.exports = nextConfig
--- pages/location/[city].js
+++ pages/location/[city].js
@@ -1,5 +1,7 @@
 import React from 'react'
 import cities from '../../lib/city.list.json'
+import Head from 'next/head';
+import TodaysWeather from '../../components/TodaysWeather';
 
 export async function getServerSideProps(context) {
     const city = getCity(context.params.city);
@@ -19,12 +21,14 @@ export async function getServerSideProps(context) {
         }
     }
 
-    const slug = context.params.city;
+    const hourlyWeather = getHourlyWeather(data.hourly);
 
     return {
         props: {
-            slug: slug,
-            data: data,
+            city: city,
+            currentWeather: data.current,
+            dailyWeather: data.daily,
+            hourlyWeather: hourlyWeather,
         }
     }
 }
@@ -48,11 +52,35 @@ const getCity = (param) => {
     }
 }
 
-const City = ({ slug, data }) => {
-    console.log(data);
+const getHourlyWeather = (hourlyData) => {
+    const current = new Date();
+    current.setHours(current.getHours(), 0, 0, 0);
+
+    const tomorrow = new Date(current);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    tomorrow.setHours(0, 0, 0, 0);
+
+    const currentTimeStamp = Math.floor(current.getTime() / 1000);
+    const tomorrowTimeStamp = Math.floor(tomorrow.getTime() / 1000);
+
+    const todaysData = hourlyData.filter(data => data.dt < tomorrowTimeStamp);
+
+    return todaysData;
+}
+
+const City = ({ hourlyWeather, city, currentWeather, dailyWeather }) => {
+
     return (
         <div>
-            <h2>{slug}</h2>
+            <Head>
+                <title>{city.name} Weather</title>
+            </Head>
+            
+            <div className="page-wrapper">
+                <div className="container">
+                    <TodaysWeather city={city} weather={dailyWeather[0]} />
+                </div>
+            </div>
         </div>
     )
 }
