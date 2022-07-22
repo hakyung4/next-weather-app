f610ed0ae1abdc7486c753f260005229db9ae010
hakyung4 <hakyung4@illinois.edu>
2022-07-22 13:59:28 -0500

    openweathermap api

--- pages/location/[city].js
+++ pages/location/[city].js
@@ -1,9 +1,58 @@
 import React from 'react'
+import cities from '../../lib/city.list.json'
 
-const City = () => {
+export async function getServerSideProps(context) {
+    const city = getCity(context.params.city);
+    
+    if (!city) {
+        return {
+            notFound: true,
+        }
+    }
+
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${city.coord.lat}&lon=${city.coord.lon}&exclude=minutely&appid=${process.env.API_KEY}&units=metric`);
+    const data = await res.json();
+
+    if (!data) {
+        return {
+            notFound: true
+        }
+    }
+
+    const slug = context.params.city;
+
+    return {
+        props: {
+            slug: slug,
+            data: data,
+        }
+    }
+}
+
+const getCity = (param) => {
+    const cityParam = param.trim();
+
+    const splitCity = cityParam.split("-");
+    const id = splitCity[splitCity.length - 1];
+    
+    if (!id) {
+        return null;
+    }
+
+    const city = cities.find(city => city.id.toString() == id);
+
+    if (city) {
+        return city;
+    } else {
+        return null;
+    }
+}
+
+const City = ({ slug, data }) => {
+    console.log(data);
     return (
         <div>
-            City
+            <h2>{slug}</h2>
         </div>
     )
 }
